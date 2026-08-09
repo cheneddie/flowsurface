55889c7b53b0c3beeafb118743c2d25c4e6c90a7
Wei Chi Chen (陳瑋棋) <eddie067@hotmail.com>
2026-08-10 01:01:04 +0800

    feat(orderflow): export book consumption detector

--- data/src/orderflow/mod.rs
+++ data/src/orderflow/mod.rs
@@ -1,6 +1,8 @@
+pub mod book_consumption;
 pub mod speed;
 pub mod window;
 
+pub use book_consumption::{BookConsumptionDetector, ConsumptionConfig};
 pub use speed::{
     BookConsumptionEvent, BookSide, BookSpeedEngine, BookSpeedSnapshot, SpeedConfig,
     TradeSpeedEngine, TradeSpeedSnapshot,
